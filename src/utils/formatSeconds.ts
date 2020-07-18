c7c57e436f9a0d83e1358ec64831a1d194c982d6
furetur <furetur@gmail.com>
2020-07-18 22:36:42 +0500

    fixed formatSeconds to return 0:01 instead of 0:1

--- src/utils/formatSeconds.ts
+++ src/utils/formatSeconds.ts
@@ -1,7 +1,7 @@
 const formatSeconds = (seconds: number): string => {
 	const minutes = Math.floor(seconds / 60);
 	const freeSeconds = seconds - minutes * 60;
-	return `${minutes}:${freeSeconds}`;
+	return `${minutes}:${freeSeconds <= 9 ? `0${freeSeconds}` : freeSeconds}`;
 };
 
 export default formatSeconds;
